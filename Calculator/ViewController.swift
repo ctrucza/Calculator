446370c5da29ffb31e9b6a392c5b7f2bae4c2ed2
ctrucza <csaba.trucza@gmail.com>
2015-02-08 16:11:03 +0200

    and you don't even need to name the parameters

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
         }
         
         switch operation {
-        case "×": performOperation({(op1, op2) in op1 * op2 })
+        case "×": performOperation({ $0 * $1 })
 //        case "÷":
 //        case "+":
 //        case "−":
