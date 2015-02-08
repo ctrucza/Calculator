031def18aa620054c028cd120a6dbb8bd4f05a8c
ctrucza <csaba.trucza@gmail.com>
2015-02-08 15:20:39 +0200

    end of lecture 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
 }
 
